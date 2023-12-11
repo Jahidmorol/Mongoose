1298d2c5622e0c295d0d8aeed152ee6fb4ecdbe6
jahid <jahidmorol2@gmail.com>
2023-12-11 22:09:54 +0600

    module 13.9 is done

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from 'mongoose';
 import { Student } from './student.model';
+import appError from '../../errors/appError';
+import httpStatus from 'http-status';
+import { User } from '../user/user.model';
 
 const getAllStudentFromDb = async () => {
   const result = await Student.find()
@@ -25,8 +29,42 @@ const getSingleStudentFromDb = async (id: string) => {
 };
 
 const deleteStudentFromDb = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true });
-  return result;
+  // const result = await Student.updateOne({ id }, { isDeleted: true });
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const deleteStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { upsert: true, session },
+    );
+
+    if (!deleteStudent) {
+      throw new appError(httpStatus.BAD_REQUEST, 'Failed to delete student');
+    }
+
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { upsert: true, session },
+    );
+
+    if (!deletedUser) {
+      throw new appError(httpStatus.BAD_REQUEST, 'Failed to delete User');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return deleteStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error('Failed to delete student');
+  }
 };
 
 export const studentServices = {
