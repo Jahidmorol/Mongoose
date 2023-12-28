759a70c31fe70a0fcd2e895a853598936e9a5df3
jahid <jahidmorol2@gmail.com>
2023-12-28 13:13:21 +0600

    milstoon4 is starting and project-8 is finished

---
+++ src/app/modules/OfferedCourse/OfferedCourse.controller.ts
@@ -0,0 +1,71 @@
+import httpStatus from 'http-status';
+import sendResponse from '../../utils/sendResponse';
+import { OfferedCourseServices } from './OfferedCourse.service';
+import catchAsync from '../../utils/catchAsync';
+
+const createOfferedCourse = catchAsync(async (req, res) => {
+  const result = await OfferedCourseServices.createOfferedCourseIntoDB(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Offered Course is created successfully !',
+    data: result,
+  });
+});
+
+const getAllOfferedCourses = catchAsync(async (req, res) => {
+  const result = await OfferedCourseServices.getAllOfferedCoursesFromDB(
+    req.query,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourses retrieved successfully !',
+    data: result,
+  });
+});
+
+const getSingleOfferedCourses = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OfferedCourseServices.getSingleOfferedCourseFromDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'OfferedCourse fetched successfully',
+    data: result,
+  });
+});
+
+const updateOfferedCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+
+  const result = await OfferedCourseServices.updateOfferedCourseIntoDB(
+    id,
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'OfferedCourse updated successfully',
+    data: result,
+  });
+});
+
+const deleteOfferedCourseFromDB = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OfferedCourseServices.deleteOfferedCourseFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'OfferedCourse deleted successfully',
+    data: result,
+  });
+});
+
+export const OfferedCourseControllers = {
+  createOfferedCourse,
+  getAllOfferedCourses,
+  getSingleOfferedCourses,
+  updateOfferedCourse,
+  deleteOfferedCourseFromDB,
+};
